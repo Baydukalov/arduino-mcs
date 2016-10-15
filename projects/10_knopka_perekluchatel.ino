77e3f4f55f44a96c72ee9aaa53eadbcb93e935bb
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2016-10-15 21:57:57 +0300

    обновление проектов

--- projects/10_knopka_perekluchatel.ino
+++ projects/10_knopka_perekluchatel.ino
@@ -1,6 +1,6 @@
 // схема https://rm-content.s3.amazonaws.com/57024268467568d777fa4a53/623356/upload-b05cc610-9175-11e6-85c6-95b0dea6d9af.png
 
-#define BUTTON_PIN 3
+#define BUTTON_PIN 2
 #define LED_PIN 13
 
 boolean buttonWasUp = true;  // была ли кнопка отпущена?
@@ -8,8 +8,8 @@ boolean ledEnabled = false;  // включен ли свет?
 
 void setup()
 {
-  pinMode(13, OUTPUT);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop()
@@ -17,7 +17,7 @@ void loop()
   // определить момент «клика» несколько сложнее, чем факт того,
   // что кнопка сейчас просто нажата. Для определения клика мы
   // сначала понимаем, отпущена ли кнопка прямо сейчас...
-  boolean buttonIsUp = digitalRead(3);
+  boolean buttonIsUp = digitalRead(BUTTON_PIN);
 
   // ...если «кнопка была отпущена и (&&) не отпущена сейчас»...
   if (buttonWasUp && !buttonIsUp) {
@@ -26,11 +26,11 @@ void loop()
     // поэтому даём кнопке полностью «успокоиться»...
     delay(10);
     // ...и считываем сигнал снова
-    buttonIsUp = digitalRead(3);
+    buttonIsUp = digitalRead(BUTTON_PIN);
     if (!buttonIsUp) {  // если она всё ещё нажата...
       // ...это клик! Переворачиваем сигнал светодиода
       ledEnabled = !ledEnabled;
-      digitalWrite(13, ledEnabled);
+      digitalWrite(LED_PIN, ledEnabled);
     }
   }
 
--- projects/9_lampa_i_knopka.ino
+++ projects/9_lampa_i_knopka.ino
@@ -3,13 +3,12 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   boolean keyUp = digitalRead(2);
-  Serial.println(keyUp);
   if (!keyUp) {
     digitalWrite(13, 1);
   }
